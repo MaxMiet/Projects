40a45d9077eb985e12992c0e0ffc9c88058c0995
MaxMiet <86667885+MaxMiet@users.noreply.github.com>
2021-06-29 16:43:15 +0300

    uploaded Sleepify software code

---
+++ Sleepify/Sleepify/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace Sleepify
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.metroButton2 = new MetroFramework.Controls.MetroButton();
+            this.mLabelH = new MetroFramework.Controls.MetroLabel();
+            this.mLabelM = new MetroFramework.Controls.MetroLabel();
+            this.mLabelS = new MetroFramework.Controls.MetroLabel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("CircularStd-Bold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown1.ForeColor = System.Drawing.Color.White;
+            this.numericUpDown1.Location = new System.Drawing.Point(65, 81);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(41, 25);
+            this.numericUpDown1.TabIndex = 3;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Font = new System.Drawing.Font("CircularStd-Bold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown2.ForeColor = System.Drawing.Color.White;
+            this.numericUpDown2.Location = new System.Drawing.Point(136, 81);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(41, 25);
+            this.numericUpDown2.TabIndex = 4;
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Cursor = System.Windows.Forms.Cursors.Default;
+            this.numericUpDown3.Font = new System.Drawing.Font("CircularStd-Bold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown3.ForeColor = System.Drawing.Color.White;
+            this.numericUpDown3.Location = new System.Drawing.Point(215, 81);
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(41, 25);
+            this.numericUpDown3.TabIndex = 5;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(291, 218);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 14);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Help";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Highlight = true;
+            this.metroButton1.Location = new System.Drawing.Point(73, 190);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(75, 23);
+            this.metroButton1.Style = MetroFramework.MetroColorStyle.Green;
+            this.metroButton1.TabIndex = 9;
+            this.metroButton1.Text = "Start";
+            this.metroButton1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // metroButton2
+            // 
+            this.metroButton2.Highlight = true;
+            this.metroButton2.Location = new System.Drawing.Point(169, 190);
+            this.metroButton2.Name = "metroButton2";
+            this.metroButton2.Size = new System.Drawing.Size(75, 23);
+            this.metroButton2.Style = MetroFramework.MetroColorStyle.Green;
+            this.metroButton2.TabIndex = 10;
+            this.metroButton2.Text = "Stop";
+            this.metroButton2.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroButton2.Click += new System.EventHandler(this.metroButton2_Click);
+            // 
+            // mLabelH
+            // 
+            this.mLabelH.AutoSize = true;
+            this.mLabelH.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.mLabelH.Location = new System.Drawing.Point(57, 109);
+            this.mLabelH.Name = "mLabelH";
+            this.mLabelH.Size = new System.Drawing.Size(57, 25);
+            this.mLabelH.Style = MetroFramework.MetroColorStyle.Green;
+            this.mLabelH.TabIndex = 11;
+            this.mLabelH.Text = "Hours";
+            this.mLabelH.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.mLabelH.UseStyleColors = true;
+            // 
+            // mLabelM
+            // 
+            this.mLabelM.AutoSize = true;
+            this.mLabelM.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.mLabelM.Location = new System.Drawing.Point(120, 109);
+            this.mLabelM.Name = "mLabelM";
+            this.mLabelM.Size = new System.Drawing.Size(72, 25);
+            this.mLabelM.Style = MetroFramework.MetroColorStyle.Green;
+            this.mLabelM.TabIndex = 12;
+            this.mLabelM.Text = "Minutes";
+            this.mLabelM.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.mLabelM.UseStyleColors = true;
+            // 
+            // mLabelS
+            // 
+            this.mLabelS.AutoSize = true;
+            this.mLabelS.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.mLabelS.Location = new System.Drawing.Point(198, 109);
+            this.mLabelS.Name = "mLabelS";
+            this.mLabelS.Size = new System.Drawing.Size(75, 25);
+            this.mLabelS.Style = MetroFramework.MetroColorStyle.Green;
+            this.mLabelS.TabIndex = 13;
+            this.mLabelS.Text = "Seconds";
+            this.mLabelS.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.mLabelS.UseStyleColors = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(276, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(45, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 14;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(327, 237);
+            this.ControlBox = false;
+            this.Controls.Add(this.mLabelS);
+            this.Controls.Add(this.mLabelM);
+            this.Controls.Add(this.mLabelH);
+            this.Controls.Add(this.metroButton2);
+            this.Controls.Add(this.metroButton1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("CircularStd-Bold", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Resizable = false;
+            this.Style = MetroFramework.MetroColorStyle.Green;
+            this.Text = "Sleepify";
+            this.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private MetroFramework.Controls.MetroButton metroButton2;
+        private MetroFramework.Controls.MetroLabel mLabelH;
+        private MetroFramework.Controls.MetroLabel mLabelM;
+        private MetroFramework.Controls.MetroLabel mLabelS;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Sleepify/Sleepify/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+//using System.Timers;
+
+namespace Sleepify
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        private int hours;
+        private int minutes;
+        private int seconds;
+        private int trig = 0;
+
+        public const int KEYEVENTF_EXTENTEDKEY = 1;
+        public const int KEYEVENTF_KEYUP = 0;
+        public const int VK_MEDIA_NEXT_TRACK = 0xB0;// code to switch to next song
+        public const int VK_MEDIA_PLAY_PAUSE = 0xB3;// code to play or pause a song
+        public const int VK_MEDIA_PREV_TRACK = 0xB1;// code to switch to prev song
+
+        [DllImport("user32.dll")]
+        public static extern void keybd_event(byte virtualKey, byte scanCode, uint flags, IntPtr extraInfo);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            mLabelH.Font = new System.Drawing.Font(mLabelH.Font.Name, 10F);
+            mLabelM.Font = new System.Drawing.Font(mLabelM.Font.Name, 10F);
+            mLabelS.Font = new System.Drawing.Font(mLabelS.Font.Name, 10F);
+
+        }
+
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+            hours = Convert.ToInt32(Math.Round(numericUpDown1.Value, 0));
+            minutes = Convert.ToInt32(Math.Round(numericUpDown2.Value, 0));
+            seconds = Convert.ToInt32(Math.Round(numericUpDown3.Value, 0));
+
+            mLabelH.Text = hours.ToString();
+            mLabelM.Text = minutes.ToString();
+            mLabelS.Text = seconds.ToString();
+            if (trig == 0)
+            {
+                timer1 = new Timer();
+                timer1.Tick += new EventHandler(timer1_Tick);
+                timer1.Interval = 1000; // 1 sec
+                trig = 1;
+                //timer1.Start();
+            }
+            timer1.Start();
+        }
+
+        private void metroButton2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            hours = 0;
+            minutes = 0;
+            seconds = 0;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //verify if the time passed or not
+            if ((minutes == 0) && (hours == 0) && (seconds == 0))
+            {
+                //if the time is done, reset all settings and fields
+                //show the message, notifying that the time is over
+                //timer1.Enabled = false;
+                timer1.Stop();
+                keybd_event(VK_MEDIA_PLAY_PAUSE, 0, KEYEVENTF_EXTENTEDKEY, IntPtr.Zero);
+                MessageBox.Show("Music has been paused.", "Sleepify");
+                numericUpDown1.Value = 0;
+                numericUpDown2.Value = 0;
+                numericUpDown3.Value = 0;
+
+            }
+            else
+            {
+                //else continue counting.
+                if (seconds < 1)
+                {
+                    seconds = 59;
+                    if (minutes == 0)
+                    {
+                        minutes = 59;
+                        if (hours != 0)
+                            hours -= 1;
+
+                    }
+                    else
+                    {
+                        minutes -= 1;
+                    }
+                }
+                else
+                    seconds -= 1;
+                //display the current values of hours, minutes and seconds in
+                //the corresponding fields
+                mLabelH.Text = hours.ToString();
+                mLabelM.Text = minutes.ToString();
+                mLabelS.Text = seconds.ToString();
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Have your music already playing when pressing Start.", "Basiq:");
+        }
+    }
+}
